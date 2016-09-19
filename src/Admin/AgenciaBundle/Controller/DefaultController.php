786d2bbc1f6137e66428a234f92ff07e10296069
U-Family-PC\Family <luisk__@hotmail.com>
2016-09-18 20:44:19 -0500

    yml

--- src/Admin/AgenciaBundle/Controller/DefaultController.php
+++ src/Admin/AgenciaBundle/Controller/DefaultController.php
@@ -282,4 +282,94 @@ class DefaultController extends Controller {
         ));
     }
 
+    /**
+     * recibe y envia el problema a el email del desarrollador del sistema
+     *
+     * @Route("/Agencia/dashboard/reportar/problema", name="Agencia_reportarproblema")
+     * @Method("GET")
+     * @Template("AdminAgenciaBundle:views:reportarproblemas.html.twig")
+     */
+    public function reportarproblemasAction() {
+        $security_context = $this->get('security.context');
+        $security_token = $security_context->getToken();
+        //definimos el usuario, con rol diferentea cordinador, administrador,suberadmin,usuario
+        $user = $security_token->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+                        'SELECT au
+                        FROM AdminAdminBundle:AgenciaUsuario au
+                        WHERE au.idUsuario  =:id'
+                )->setParameter('id', $user->getId());
+        if ($query->getResult()) {
+            $Agencia = $query->getResult();
+            // Buscamos el array de resultados
+            $Agencia = $query->setMaxResults(1)->getOneOrNullResult();
+//verificamos que plan tiene asignado la agencia
+            $Agenciaplan = DashboardController::agenciaplan($Agencia->getIdAgencia()->getId());
+        } else {
+            $Agenciaplan = null;
+            $Agencia = null;
+        }
+        return array('AgenciaP' => $Agenciaplan);
+    }
+
+    /**
+     * envia el problema a el email del desarrollador del sistema
+     *
+     * @Route("/Agencia/dashboard/reportar/problema/send", name="Agencia_enviarproblema")
+     * @Method("GET")
+     * @Template("AdminAgenciaBundle:views:sendproblemas.html.twig")
+     */
+    public function sendproblemaAction(Request $request) {
+        $security_context = $this->get('security.context');
+        $security_token = $security_context->getToken();
+        //definimos el usuario, con rol diferentea cordinador, administrador,suberadmin,usuario
+        $user = $security_token->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+                        'SELECT au
+                        FROM AdminAdminBundle:AgenciaUsuario au
+                        WHERE au.idUsuario  =:id'
+                )->setParameter('id', $user->getId());
+        if ($query->getResult()) {
+            $Agencia = $query->getResult();
+            // Buscamos el array de resultados
+            $Agencia = $query->setMaxResults(1)->getOneOrNullResult();
+//verificamos que plan tiene asignado la agencia
+            $Agenciaplan = DashboardController::agenciaplan($Agencia->getIdAgencia()->getId());
+        } else {
+            $Agenciaplan = null;
+            $Agencia = null;
+        }
+        ///procedemos a enviar el email
+        $asunto = $request->query->get('asunto');
+
+        $Body = $request->query->get('mensaje');
+        $correo_remitente = 'youfikner@gmail.com';
+        $Subject = 'Fikner - ' . $Agencia->getIdAgencia()->getNombreAgencia() . ':' . $asunto;
+        $emailagencia = $Agencia->getIdAgencia()->getEmail();
+        $nomagencia = $Agencia->getIdAgencia()->getNombreAgencia();
+        $email = 'luisk__@hotmail.com';
+        $message = \Swift_Message::newInstance()
+                ->setSubject($Subject)
+                ->setFrom($correo_remitente)
+                ->setTo($email)
+                ->setBody(
+                <<<EOF
+                Asunto:  $Subject
+                Correo:  $email
+                Agencia: $nomagencia
+                
+                        $Body               
+                
+                Responda este email a $emailagencia
+                
+EOF
+                )
+        ;
+        $this->get('mailer')->send($message);
+
+        return array('AgenciaP' => $Agenciaplan);
+    }
+
 }
