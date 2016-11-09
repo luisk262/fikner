600f6108baca443f1ca6bcb754bdcf4eba6b3b9e
luisk262 <luisk__@hotmail.com>
2016-11-08 21:46:09 -0500

    se hace vista books
    se hace controlador y vista para books
    fase beta sin completar

--- src/Admin/AdminBundle/Controller/DefaultController.php
+++ src/Admin/AdminBundle/Controller/DefaultController.php
@@ -9,27 +9,28 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Admin\AdminBundle\Entity\User;
 use Admin\MyaccountBundle\Form\RegistrationType;
+use Admin\AdminBundle\Pagination\Paginator;
 
 class DefaultController extends Controller {
 
     /**
      * @Route("/", name="principal")
-     *@Method("GET")
+     * @Method("GET")
      * @Template()
      */
     public function indexAction() {
         $security_context = $this->get('security.context');
-        if(($this->get('security.context')->isGranted('ROLE_RECLU'))){
-             return $this->redirect($this->generateUrl('reclutador_dashboard'));
+        if (($this->get('security.context')->isGranted('ROLE_RECLU'))) {
+            return $this->redirect($this->generateUrl('reclutador_dashboard'));
         }
-        if ($this->get('security.context')->isGranted('ROLE_AGENC')){
-            return $this->redirect($this->generateUrl('agencia_dashboard'));            
+        if ($this->get('security.context')->isGranted('ROLE_AGENC')) {
+            return $this->redirect($this->generateUrl('agencia_dashboard'));
         }
         if ($this->get('security.context')->isGranted('ROLE_SUPER_ADMIN')) {
             return $this->redirect($this->generateUrl('admin_dashboard'));
         }
-        if(($this->get('security.context')->isGranted('ROLE_USER'))){
-             return $this->redirect($this->generateUrl('Myaccount'));
+        if (($this->get('security.context')->isGranted('ROLE_USER'))) {
+            return $this->redirect($this->generateUrl('Myaccount'));
         }
         $entity = new User();
         $form = $this->createCreateForm($entity);
@@ -39,7 +40,8 @@ class DefaultController extends Controller {
             'form' => $form->createView(),
         );
     }
-     /**
+
+    /**
      * Creates a form to create a User entity.
      *
      * @param User $entity The entity
@@ -51,33 +53,34 @@ class DefaultController extends Controller {
             'action' => $this->generateUrl('default_registration_create'),
             'method' => 'POST',
         ));
-        $form->add('nombre', 'text', array('label' => 'Nombres*','max_length'=>30));
-        $form->add('apellidos', 'text', array('label' => 'Apellidos*','max_length'=>30));
-        $form->add('telefono', 'text', array('max_length'=>13,'label' => 'Telefono*'));
+        $form->add('nombre', 'text', array('label' => 'Nombres*', 'max_length' => 30));
+        $form->add('apellidos', 'text', array('label' => 'Apellidos*', 'max_length' => 30));
+        $form->add('telefono', 'text', array('max_length' => 13, 'label' => 'Telefono*'));
         $form->add('fechanaci', 'date', array(
-                    'years' => range(date('Y') - 7,date('Y')- 95),
-                    'required' => True,
-                    'label' => 'Fecha de nacimimiento',
-                    'empty_value' => array('year' => 'Año', 'month' => 'Mes', 'day' => 'Dia'),
-                ));
+            'years' => range(date('Y') - 7, date('Y') - 95),
+            'required' => True,
+            'label' => 'Fecha de nacimimiento',
+            'empty_value' => array('year' => 'Año', 'month' => 'Mes', 'day' => 'Dia'),
+        ));
         $form->add('email', 'repeated', array(
             'type' => 'email',
             'options' => array('translation_domain' => 'FOSUserBundle'),
             'invalid_message' => 'Los dos correos no coinciden',
-            'first_options'  => array('label' => 'Email', 'attr'=>array('placeholder' => 'Email')),
-            'second_options' => array('label' => 'Repita Email', 'attr'=>array('placeholder' => 'Confirme Email')),
+            'first_options' => array('label' => 'Email', 'attr' => array('placeholder' => 'Email')),
+            'second_options' => array('label' => 'Repita Email', 'attr' => array('placeholder' => 'Confirme Email')),
         ));
         $form->add('plainPassword', 'repeated', array(
             'type' => 'password',
             'options' => array('translation_domain' => 'FOSUserBundle'),
-            'invalid_message' => 'fos_user.password.mismatch',            
-            'first_options'  => array('label' => 'Contraseña', 'attr'=>array('placeholder' => 'Contraseña')),
-            'second_options' => array('label' => 'Confirme Contraseña', 'attr'=>array('placeholder' => 'Confirme Contraseña'))
+            'invalid_message' => 'fos_user.password.mismatch',
+            'first_options' => array('label' => 'Contraseña', 'attr' => array('placeholder' => 'Contraseña')),
+            'second_options' => array('label' => 'Confirme Contraseña', 'attr' => array('placeholder' => 'Confirme Contraseña'))
         ));
         $form->add('submit', 'submit', array('label' => '¡CREAR CUENTA!'));
 
         return $form;
     }
+
     /**
      * Creates a new User entity.
      *
@@ -98,25 +101,25 @@ class DefaultController extends Controller {
                         WHERE u.username  =:username'
                     )->setParameter('username', $entity->getEmail());
             if ($query->getResult()) {
-                $error=true;
+                $error = true;
                 return $this->redirect(
                                 $this->generateUrl('registration_msg', array('id' => $entity->getId(),
                                     'nombre' => $entity->getNombre(),
                                     'apellidos' => $entity->getApellidos(),
-                                    'error'=>$error,
+                                    'error' => $error,
                                     'email' => $entity->getEmail()
-                ))); 
+                )));
             } else {
                 $entity->setUsername($entity->getEmail());
                 $entity->setEnabled(true);
                 $em->persist($entity);
                 $em->flush();
-                $error=false;
+                $error = false;
                 return $this->redirect(
                                 $this->generateUrl('registration_msg', array('id' => $entity->getId(),
                                     'nombre' => $entity->getNombre(),
                                     'apellidos' => $entity->getApellidos(),
-                                    'error'=>$error,
+                                    'error' => $error,
                                     'email' => $entity->getEmail()
                 )));
             }
@@ -147,18 +150,18 @@ class DefaultController extends Controller {
 
             /////////Agencias
             $queryAgencia = $em->createQueryBuilder()
-                    ->select('COUNT(a)')
-                    ->from('AdminAdminBundle:Agencia', 'a')
-                    ->andWhere('a.Activo =:estado')->setParameter('estado','1');
+                            ->select('COUNT(a)')
+                            ->from('AdminAdminBundle:Agencia', 'a')
+                            ->andWhere('a.Activo =:estado')->setParameter('estado', '1');
             $queryAgenciaP = $em->createQueryBuilder()
-                    ->select('COUNT(a)')
-                    ->from('AdminAdminBundle:Agencia', 'a')
-                    ->andWhere('a.Activo =:estado')->setParameter('estado','0');
-            
+                            ->select('COUNT(a)')
+                            ->from('AdminAdminBundle:Agencia', 'a')
+                            ->andWhere('a.Activo =:estado')->setParameter('estado', '0');
+
             $queryAgenciaU = $em->createQueryBuilder()
-                    ->select('COUNT(c)')
-                    ->from('AdminAdminBundle:User', 'c')
-                    ->andWhere('c.roles  LIKE :roles')->setParameter('roles', '%' . 'ROLE_AGENC' . '%');
+                            ->select('COUNT(c)')
+                            ->from('AdminAdminBundle:User', 'c')
+                            ->andWhere('c.roles  LIKE :roles')->setParameter('roles', '%' . 'ROLE_AGENC' . '%');
 
             ///personal de apoyo
             $Books = $querybooks->getQuery()->getSingleScalarResult();
@@ -234,21 +237,75 @@ class DefaultController extends Controller {
                     'entities' => $entities,
         ));
     }
-    public function formulariousuario(){
+
+    public function formulariousuario() {
         $security_context = $this->get('security.context');
-        if(($this->get('security.context')->isGranted('ROLE_RECLU'))){
-             return $this->redirect($this->generateUrl('reclutador_dashboard'));
+        if (($this->get('security.context')->isGranted('ROLE_RECLU'))) {
+            return $this->redirect($this->generateUrl('reclutador_dashboard'));
         }
-        if ($this->get('security.context')->isGranted('ROLE_AGENC')){
-            return $this->redirect($this->generateUrl('agencia_dashboard'));            
+        if ($this->get('security.context')->isGranted('ROLE_AGENC')) {
+            return $this->redirect($this->generateUrl('agencia_dashboard'));
         }
         if ($this->get('security.context')->isGranted('ROLE_SUPER_ADMIN')) {
             return $this->redirect($this->generateUrl('admin_dashboard'));
         }
-        if(($this->get('security.context')->isGranted('ROLE_USER'))){
-             return $this->redirect($this->generateUrl('Myaccount'));
+        if (($this->get('security.context')->isGranted('ROLE_USER'))) {
+            return $this->redirect($this->generateUrl('Myaccount'));
         }
+    }
+    /**
+     * @Route("/list/", name="principal_lista")
+     * @Method("GET")
+     * @Template()
+     */
+    public function booksAction() {
+          $request = $this->getRequest();
+        $page = $request->query->get('page');
+        $searchParam = $request->get('searchParam');
 
-        }
+        return array(
+            'current_page' => $page,
+            'searchParam' => $searchParam);
+    }
+     /**
+     * consulta a list Books.
+     *
+     * @Route("/list/ajax/consulta", name="principal_list_ajax")
+     * @Method("GET")
+     */
+    public function ajaxListAction(Request $request) {
+        $page = $request->query->get('page');
+        
+        $em = $this->getDoctrine()->getManager();
+        $entryQuery = $em->createQueryBuilder()
+                    ->select('hp', 'p', 'h')
+                    ->from('AdminAdminBundle:HojadevidaPhoto', 'hp')
+                    ->leftJoin('hp.idHojadevida', 'h')
+                    ->leftJoin('hp.idPhoto', 'p')
+                    ->andWhere('hp.principal =:principal')
+                    ->addOrderBy('hp.fechaupdate', 'DESC')
+                    ->setParameter('principal', '1');
+            //query aux
+            $queryaux = $em->createQueryBuilder()
+                    ->select('COUNT(hp)')
+                    ->from('AdminAdminBundle:HojadevidaPhoto', 'hp')
+                    ->leftJoin('hp.idHojadevida', 'h')
+                    ->leftJoin('hp.idPhoto', 'p')
+                    ->andWhere('hp.principal =:principal')
+                    ->addOrderBy('hp.fechaupdate', 'DESC')
+                    ->setParameter('principal', '1');
+             $total_count = $queryaux->getQuery()->getSingleScalarResult();
+        if (!empty($perPage))
+            $entryQuery->setFirstResult(($page - 1) * $perPage)->setMaxResults($perPage);
+        $entryQueryfinal = $entryQuery->getQuery();
+        //obtenemos el array de resultados
+        $entities = $entryQueryfinal->getArrayResult();
+        $pagination = (new Paginator())->setItems($total_count, 50)->setPage(1)->toArray();
+        //renderizamos la vista para mostrar las hojas de vida
+        return $this->render('AdminAdminBundle:Default:ajax_books_list.html.twig', array(
+                    'entities' => $entities,
+                    'pagination' => $pagination,
+        ));
+    }
 
 }
